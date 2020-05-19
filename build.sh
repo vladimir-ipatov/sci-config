78909be673a5a78d9ec5ffa574686f5569b49450
Vladimir Ipatov <vs@ipatov.net>
2020-05-19 11:47:47 +0300

    add websockify restart hook, add sci-web vncpassword set hook, add sci-mond htpasswd set hook

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 rm -f ./build
-tar czf ../sci-config_3.0.orig.tar.gz LICENSE README.md debian/changelog debian/compat debian/control debian/copyright debian/dirs debian/install debian/rules debian/source build.sh nut ganeti upssched-cmd gnt-node-shutdown.sh ganeti-apt
+tar czf ../sci-config_3.3.orig.tar.gz LICENSE README.md debian/changelog debian/compat debian/control debian/copyright debian/dirs debian/install debian/rules debian/source build.sh nut ganeti upssched-cmd gnt-node-shutdown.sh ganeti-apt
 dpkg-buildpackage -rfakeroot
